4883e1e580ef7cf5c0288c74e8a16092bec9a7f6
affigabmag <affiliate.gabmag@gmail.com>
2025-04-14 15:48:32 +0300

    pagination modification

--- templates/frontend/pagination-view.php
+++ templates/frontend/pagination-view.php
@@ -17,26 +17,8 @@ $total_pages = ceil($total / $per_page);
     <form method="post">
         <input type="hidden" name="form_type" value="pagination">
         <?php wp_nonce_field('pagination_nonce', 'pagination_nonce'); ?>
-        
-        <div class="pagination-controls">
-            <?php if ($page > 1): ?>
-                <button type="submit" name="paged" value="<?php echo esc_attr($page - 1); ?>" class="page-prev">
-                    &laquo; <?php esc_html_e('Previous', 'custom-table-crud'); ?>
-                </button>
-            <?php endif; ?>
-            
-            <span class="pagination-info">
-                <?php echo esc_html(sprintf(__('Page %d of %d', 'custom-table-crud'), $page, $total_pages)); ?>
-            </span>
-            
-            <?php if ($page < $total_pages): ?>
-                <button type="submit" name="paged" value="<?php echo esc_attr($page + 1); ?>" class="page-next">
-                    <?php esc_html_e('Next', 'custom-table-crud'); ?> &raquo;
-                </button>
-            <?php endif; ?>
-        </div>
-        
-        <?php if ($total_pages > 2): ?>
+
+        <?php if ($total_pages > 1): ?>
             <div class="pagination-jump">
                 <label for="jump-to-page"><?php esc_html_e('Go to page:', 'custom-table-crud'); ?></label>
                 <select name="paged" id="jump-to-page" onchange="this.form.submit()">
